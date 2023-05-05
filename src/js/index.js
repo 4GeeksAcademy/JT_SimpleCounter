8ef7489a9d2e8d4b956d08a4d07991b7f3b95fec
julitar <126813769+julitar@users.noreply.github.com>
2023-05-05 18:54:42 +0000

    counter funcionando

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,37 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import { matchRoutes } from "react-router";
 
 //import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SimpleCounter (props) {
+    return (<div className="bigCounter">
+        <div className="clock">
+            
+        </div>
+        <div className="six">{props.digitSix % 10}</div>
+        <div className="five">{props.digitFive % 10}</div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
+
+let counter = 0;
+setInterval(function() {
+        const six = Math.floor(counter/100000);
+        const five = Math.floor(counter/10000);
+        const four = Math.floor(counter/1000);
+        const three = Math.floor(counter/100);
+        const two = Math.floor(counter/10);
+        const one = Math.floor(counter/1);
+
+        counter++;
+
+        ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />, document.querySelector("#app")
+        );
+    }, 1000);
+
+
